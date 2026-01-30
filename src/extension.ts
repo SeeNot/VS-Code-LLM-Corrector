eeed6d1ac794727c653892b4527e560696075427
SeeNot <142214624+SeeNot@users.noreply.github.com>
2026-01-30 16:58:06 +0200

    Add env example

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,8 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { sendPromt } from './openRouter';
 import path from 'path';
 import dotenv from 'dotenv';
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
 
   const disposable = vscode.commands.registerCommand('corrector.helloWorld', async () => {
@@ -36,5 +33,4 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() { }
